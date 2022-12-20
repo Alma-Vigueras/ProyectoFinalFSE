ea84f4abcfc0ffc2606246aaf794aa0b991abaee
octaviolr <octavio987@hotmail.com>
2022-12-20 17:14:51 -0600

    Create calculos.py

---
+++ src/interfaz/calculos.py
@@ -0,0 +1,15 @@
+# !/usr/bin/python3
+##################################################
+#
+# Autor: 
+# License: MIT
+#
+###################################################-->
+import math
+from firebase import firebase 
+
+firebase = firebase.FirebaseApplication("https://invernadero-c2130-default-rtdb.firebaseio.com/",None)
+
+tempAnt = 0
+errorp = 0
+serror = 0
