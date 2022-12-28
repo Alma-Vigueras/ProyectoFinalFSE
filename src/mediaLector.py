5f77484c9773f3b6af862612cf0ae8ac6196b8ef
Alma-Vigueras <alma.ramirez@ingenieria.unam.edu>
2022-12-28 09:26:46 -0600

    2

--- src/mediaLector.py
+++ src/mediaLector.py
@@ -16,10 +16,21 @@ from tkinter import messagebox
 
 def reproducirFotos(mymedia,tiempo):
     vlc_instance = vlc.Instance() #instancia reproductor de fotos#
-    player =  #funcion para el formato en que se ven las fotos#
-    Media = 
+    player = vlc_instance.media_list_player_new() #funcion para el formato en que se ven las fotos#
+    Media = vlc_instance.media_list_new(mymedia)
     player.set_media_list(Media)
 
     for index, name in enumerate(mymedia):
-       
+       player.play_item_at_index(index)
+	    time.sleep(tiempo) #tiempo de reproduccion
+    player.stop()
+
+def reproducirMusicaVideo(file):
+    for f in file:
+        vlc_instance = vlc.Instance()
+		player = vlc_instance.media_player_new()
+        media = vlc_instance.media_new(f)
+
+
+        player.stop()
     player.stop()
