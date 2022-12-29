0d07b58b5ebe4ff40f729f0cbed61ee4a4798252
Alma-Vigueras <71671017+Alma-Vigueras@users.noreply.github.com>
2022-12-29 08:55:51 -0600

    Actualización 3

--- src/mediaLector.py
+++ src/mediaLector.py
@@ -30,7 +30,25 @@ def reproducirMusicaVideo(file):
         vlc_instance = vlc.Instance()
 		player = vlc_instance.media_player_new()
         media = vlc_instance.media_new(f)
-
-
+        player.set_media(media)
+        player.set_fullscreen(True)
+        player-play()
+        time.sleep(1.5)
+        duration = player.get_length() / 1000
+        time.sleep(duration)
         player.stop()
     player.stop()
+
+def playUSB(directory):
+    #Guarda los nombres tipo png
+	varPhotoFiles = glob.glob(directory+"/Media/Fotos/*.jpg")#Asigna guardado en carpeta de fotos
+    #Guarda los nombres tipo mp4
+	varVideoFiles = glob.glob(directory+"/Media/Videos/*.mp4")#EN carpeta video
+	#Guardan los nombres de los archivos tipo mp3 
+	varMusicFiles = glob.glob(directory+"/Media/Musica/*.mp3")#EN carpeta musica
+
+	print('varPhotoFiles: ',varPhotoFiles)
+	print('varVideoFiles: ',varVideoFiles)
+	print('varMusicFiles: ',varMusicFiles)
+    
+	if(len(varPhotoFiles)>0):
